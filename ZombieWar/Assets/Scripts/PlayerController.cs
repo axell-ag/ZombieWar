002383271898bdbe65803084ebe354c3b0982e47
Axel <anatoly.garchik@gmail.com>
2020-12-06 13:55:39 +0300

    added singleton

---
+++ ZombieWar/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZombieWar;
+
+public class PlayerController : Singleton<PlayerController>
+{
+
+}
---
+++ ZombieWar/Assets/Scripts/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieWar
+{
+    public class Singleton<Type> : MonoBehaviour where Type: MonoBehaviour
+    {
+        private static Type _instance;
+
+        public static Type Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<Type>();
+
+                    if (_instance == null)
+                    {
+                        var singleton = new GameObject("[SINGLETON]" + typeof(Type));
+                        _instance = singleton.AddComponent<Type>();
+                        DontDestroyOnLoad(singleton);
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
